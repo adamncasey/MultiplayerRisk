a7a0f753f0cb2817016d28d128a504e3816f0835
James Russell <jr79@st-andrews.ac.uk>
2014-10-12 18:32:30 +0100

    TeamCity build failure test

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,10 @@ package main;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(Stringsareawasome[] args) {
 		System.out.println(test());
 	}
-	
+
 	public static String test() {
 		return "Hello World!";
 	}

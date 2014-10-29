cfdd57d56d3f4ef70bba4a1a30d888e2abde518a
Nathan Blades <=>
2014-10-29 22:16:19 +0000

    Added simple test for Game

---
+++ src/test/logic/GameTest.java
@@ -0,0 +1,16 @@
+package test.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import logic.Game;
+
+public class GameTest {
+
+    @Test(expected=NullPointerException.class)
+    public void initializeNullTest(){
+        Game game = new Game();
+        game.initialize(null);
+    }
+}

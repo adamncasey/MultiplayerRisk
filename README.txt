929622322d04af7ae1470b6fb587ea64569e3cdd
Nathan Blades <=>
2014-10-30 06:00:42 +0000

    Fixed ant warning, added README, added main.RunGame for testing game loop

---
+++ README.txt
@@ -0,0 +1,17 @@
+How to build the program:
+
+ant
+
+How to test the program:
+
+ant test
+
+How to run the program:
+
+- main
+java -classpath bin/RiskyBusiness.jar main.Main 
+or
+./bin/RiskyBusiness.jar
+
+- rungame
+java -classpath bin/RiskyBusiness.jar main.RunGame
---
+++ src/main/RunGame.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Scanner;
+
+public class RunGame {
+
+    public static void main(String[] args){
+        System.out.println("Running Game ...");
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter the first number> ");
+        int a = reader.nextInt();
+    }
+}

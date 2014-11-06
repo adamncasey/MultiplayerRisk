d7e01438bb8b518688df39f3773f5712eb00e570
Nathan Blades <nb48@st-andrews.ac.uk>
2014-11-06 08:34:36 +0000

    Added GameState and changed Game/SimplePlayer to make use of it

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -1,12 +1,13 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import player.IPlayer;
 import networking.Message;
 
 /**
- * Game --- The main game loop that lets each player take their turn, it links player moves to the GameState.
+ * game --- the main game loop that lets each player take their turn, it links player moves to the gamestate.
  * @author Nathan Blades
  */
 public class Game {
@@ -17,11 +18,17 @@ public class Game {
     private ArrayList<IPlayer> players;
 
    /**
+    * The current game state, it is updated by Game after each player confirms the move.
+    */
+    private GameState state;
+
+   /**
     * Configure the game for a specific set of players and settings.
     * @param players The list of IPlayers that will play the game
     */
-    public Game(ArrayList<IPlayer> players){
+    public Game(ArrayList<IPlayer> players, GameState state){
         this.players = new ArrayList<IPlayer>(players);
+        this.state = state;
     }
 
     /**
@@ -37,14 +44,29 @@ public class Game {
     * @return No return value
     */
     public void playGame(){
-        boolean play = true;
+        Random random = new Random();
+
         int activePlayer = 0;
         int numPlayers = players.size();
-        while(play){
+        while(true){
+            // The activePlayer takes their turn
        	    turn(players.get(activePlayer));
-            activePlayer = (activePlayer + 1) % numPlayers;
+
             // Code to remove eliminated players goes here
-            // Check that there is more than 1 player left (or end game)
+            // Here is an example, this game randomly removes the active player after their turn/
+            int randInt = random.nextInt(10);
+            if(randInt == 0){
+                players.remove(activePlayer);
+                numPlayers = players.size();
+            } 
+
+            // End the game when there is 1 player left
+            if(numPlayers == 1){
+                break;
+            }
+
+            // Increment activePlayer
+            activePlayer = (activePlayer + 1) % numPlayers;   
         }
     }
 
@@ -52,15 +74,19 @@ public class Game {
         // Current protocol has 4 stages per turn
         for(int i = 0; i != 4; ++i){
             Message move = activePlayer.getMove(i);
+            boolean legal = true; // Is the move legal?
             for(IPlayer p : players){
                 // Don't confirm with the player who made the move
-                if(p.getId().equals(activePlayer.getId())){
+                if(p == activePlayer){ // Reference equality should work
                     continue;
                 }
-                boolean legal = p.confirmMove(i, move);
-                if(!legal){
-                    // ??????????
-                }
+                boolean confirmation = p.confirmMove(i, move);
+                legal = legal && confirmation;
+            }
+            if(legal){
+                state.update(move);
+            } else {
+                // What do we do when a move isn't legal?
             }
         }
     }

a4d7772edac6cb98cd2d93c47d91f2ed04ef6094
Adam Casey <adamncasey@gmail.com>
2014-11-09 01:07:45 +0000

    Wrote out some network messages which will need to be parsed.
    Imported JSON library, have not actually tried using it yet though.

--- src/networking/parser/Parser.java
+++ src/networking/parser/Parser.java
@@ -3,6 +3,12 @@ package networking.parser;
 import networking.Message;
 
 public class Parser {
+	/**
+	 * Parses an entire JSON object in the format described by Json communication structure
+	 * @param jsonMessage
+	 * @return Message - The data in jsonMessage parsed into our data structure.
+	 * @throws ParserException - Thrown if there is an error parsing
+	 */
 	Message parseMessage(String jsonMessage) throws ParserException {
 		return null;
 	}
---
+++ src/test/networking/MessageParserTests.java
@@ -0,0 +1,66 @@
+package test.networking;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessageParserTests {
+
+	@Test
+	public void testBasicMessage() {
+		fail("Not yet implemented");
+		/*
+		{
+			 "command": "acknowledge",
+			 "payload":	{
+			 				"ack_id": 32183921,
+			 				"response": 0,
+			 				"data": null
+			 			}
+			 "player_id": "5",
+			 "signature": "TBD"
+		}
+		
+		{
+			 "command": "trade_in_cards",
+			 "payload":	null,
+			 "player_id": "0",
+			 "signature": "TBD"
+			 "ack_id": 67812687
+		}
+		
+		{
+			 "command": "acknowledge",
+			 "payload":	{
+			 				"ack_id": 67812687,
+			 				"response": 0,
+			 				"data": null
+			 			}
+			 			
+			 "player_id": "1",
+			 "signature": "TBD"
+		}
+		
+		{
+		    "command": "deploy",
+		    "payload": [[12, 5], [13, 10]],
+		    "ack_id": 12382,
+			"player_id": "0",
+			"signature": "TBD"
+		}
+
+		{
+			 "command": "acknowledge",
+			 "payload":	{
+			 				"ack_id": 12382,
+			 				"response": 0,
+			 				"data": null
+			 			}
+			 			
+			 "player_id": "1",
+			 "signature": "TBD"
+		}
+		 */
+	}
+
+}

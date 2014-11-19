8469e1b1a882417299c5608d6aacad0408dd5ef8
Adam Casey <adamncasey@gmail.com>
2014-11-19 06:11:48 +0000

    Some failing Network Parser Tests.

--- src/networking/parser/Parser.java
+++ src/networking/parser/Parser.java
@@ -1,5 +1,8 @@
 package networking.parser;
 
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
 import networking.Message;
 
 public class Parser {
@@ -9,7 +12,28 @@ public class Parser {
 	 * @return Message - The data in jsonMessage parsed into our data structure.
 	 * @throws ParserException - Thrown if there is an error parsing
 	 */
-	Message parseMessage(String jsonMessage) throws ParserException {
-		return null;
+	public static Message parseMessage(String jsonMessage) throws ParserException {
+
+		Object parsed;
+
+		try {
+			parsed = JSONValue.parseWithException(jsonMessage);
+		} catch (ParseException e) {
+			throw new ParserException("Unable to parse packet. Error: " + e.getMessage() + ". Message:'" + jsonMessage + "'");
+		}
+
+		return parseJSONValueToMessage(parsed);
+	}
+
+	/**
+	 * Parses the JSON structure into Message.
+	 * @param  parsed
+	 * @return Message object on success
+	 * @throws ParserException The JSON input could not be parsed to a Message
+	 */
+	private static Message parseJSONValueToMessage(Object parsed) throws ParserException {
+		throw new ParserException("Unsupported Message type");
+		
+		//return null;
 	}
 }
--- src/test/networking/MessageParserTests.java
+++ src/test/networking/MessageParserTests.java
@@ -1,14 +1,15 @@
 package test.networking;
 
 import static org.junit.Assert.*;
+import networking.parser.Parser;
+import networking.parser.ParserException;
 
 import org.junit.Test;
 
 public class MessageParserTests {
 
 	@Test
-	public void testBasicMessage() {
-		fail("Not yet implemented");
+	public void testAcknowledgeMessage() throws ParserException {
 		/*
 		{
 			 "command": "acknowledge",
@@ -20,7 +21,24 @@ public class MessageParserTests {
 			 "player_id": "5",
 			 "signature": "TBD"
 		}
+		*/
+		String message = "{\r\n" + 
+				"			 \"command\": \"acknowledge\",\r\n" + 
+				"			 \"payload\":	{\r\n" + 
+				"			 				\"ack_id\": 32183921,\r\n" + 
+				"			 				\"response\": 0,\r\n" + 
+				"			 				\"data\": null\r\n" + 
+				"			 			}\r\n" + 
+				"			 \"player_id\": \"5\",\r\n" + 
+				"			 \"signature\": \"TBD\"\r\n" + 
+				"		}";
 		
+		assertNotNull(Parser.parseMessage(message));
+	}
+	
+	@Test
+	public void testTradeInCards() throws ParserException {
+		/*
 		{
 			 "command": "trade_in_cards",
 			 "payload":	null,
@@ -28,7 +46,21 @@ public class MessageParserTests {
 			 "signature": "TBD"
 			 "ack_id": 67812687
 		}
+		*/
+		String message = "	{\r\n" + 
+				"		 \"command\": \"trade_in_cards\",\r\n" + 
+				"		 \"payload\":	null,\r\n" + 
+				"		 \"player_id\": \"0\",\r\n" + 
+				"		 \"signature\": \"TBD\"\r\n" + 
+				"		 \"ack_id\": 67812687\r\n" + 
+				"	}";
 		
+		assertNotNull(Parser.parseMessage(message));
+	}
+	
+	@Test
+	public void testAcknowledge2() throws ParserException {
+		/*
 		{
 			 "command": "acknowledge",
 			 "payload":	{
@@ -40,7 +72,25 @@ public class MessageParserTests {
 			 "player_id": "1",
 			 "signature": "TBD"
 		}
+		*/
+		String message = "		{\r\n" + 
+				"		 \"command\": \"acknowledge\",\r\n" + 
+				"		 \"payload\":	{\r\n" + 
+				"		 				\"ack_id\": 67812687,\r\n" + 
+				"		 				\"response\": 0,\r\n" + 
+				"		 				\"data\": null\r\n" + 
+				"		 			}\r\n" + 
+				"		 			\r\n" + 
+				"		 \"player_id\": \"1\",\r\n" + 
+				"		 \"signature\": \"TBD\"\r\n" + 
+				"	}";
 		
+		assertNotNull(Parser.parseMessage(message));
+	}
+	
+	@Test
+	public void testDeploy() throws ParserException {
+		/*
 		{
 		    "command": "deploy",
 		    "payload": [[12, 5], [13, 10]],
@@ -48,19 +98,16 @@ public class MessageParserTests {
 			"player_id": "0",
 			"signature": "TBD"
 		}
-
-		{
-			 "command": "acknowledge",
-			 "payload":	{
-			 				"ack_id": 12382,
-			 				"response": 0,
-			 				"data": null
-			 			}
-			 			
-			 "player_id": "1",
-			 "signature": "TBD"
-		}
-		 */
+		*/
+		String message = "	{\r\n" + 
+				"	    \"command\": \"deploy\",\r\n" + 
+				"	    \"payload\": [[12, 5], [13, 10]],\r\n" + 
+				"	    \"ack_id\": 12382,\r\n" + 
+				"		\"player_id\": \"0\",\r\n" + 
+				"		\"signature\": \"TBD\"\r\n" + 
+				"	}";
+		
+		assertNotNull(Parser.parseMessage(message));
 	}
 
 }

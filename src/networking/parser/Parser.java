7de7c504910a7128836c6d167ee25e8258924e7e
Adam Casey <adamncasey@gmail.com>
2014-11-20 23:14:06 +0000

    Basic parsing of a number of message types is now complete. + Testing
    The payload is not parsed beyond ensuring it is valid JSON. Need to discuss what format other parts of the game would like for this.

--- src/networking/parser/Parser.java
+++ src/networking/parser/Parser.java
@@ -1,8 +1,10 @@
 package networking.parser;
 
+import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.ParseException;
 
+import networking.Command;
 import networking.Message;
 
 public class Parser {
@@ -19,7 +21,7 @@ public class Parser {
 		try {
 			parsed = JSONValue.parseWithException(jsonMessage);
 		} catch (ParseException e) {
-			throw new ParserException("Unable to parse packet. Error: " + e.getMessage() + ". Message:'" + jsonMessage + "'");
+			throw new ParserException("Unable to parse packet. Error: " + e.toString() + ". Message:'" + jsonMessage + "'");
 		}
 
 		return parseJSONValueToMessage(parsed);
@@ -32,8 +34,72 @@ public class Parser {
 	 * @throws ParserException The JSON input could not be parsed to a Message
 	 */
 	private static Message parseJSONValueToMessage(Object parsed) throws ParserException {
-		throw new ParserException("Unsupported Message type");
+		if(!(parsed instanceof JSONObject)) {
+			throw new ParserException("Invalid message format. Expecting JSONObject");
+		}
 		
-		//return null;
+		JSONObject message = (JSONObject)parsed;
+		
+		validateMessageFields(message);
+		
+		Command command = Command.parse((String) message.get("command"));
+		
+		// TODO: Perhaps this switch statement could process payload, and be in function rather than here.
+		switch(command) {
+		
+		case ACKNOWLEDGEMENT:
+		case JOIN:
+			// payload = Object.
+				// supported_versions
+				// supported_features
+		case JOIN_ACCEPT:
+			// payload = Object
+				// player_id
+				// acknowledgement_timeout
+				// move_timeout
+		case DEPLOY:
+		case ATTACK:
+		case CAPTURE:
+		case FORTIFY:
+			// payload can be null.
+			
+		case TRADE_IN_CARDS:
+			// payload can be null.
+			break;
+			
+		default:
+			throw new ParserException("Unsupported Message type. " + command);
+		}
+		
+		// TODO Signature check.
+		
+		return new Message(command, false, (long) message.get("player_id"), message.get("payload"));
+	}
+	
+	/**
+	 * Checks the JSONObject conforms at a basic level to 2. Json communication structure
+	 * Ensures existance of fields and correct type of fields
+	 * 
+	 * TODO: Here might be a good time to verify signatures
+	 * 
+	 * @param message
+	 * @throws ParserException
+	 */
+	private static void validateMessageFields(JSONObject message) throws ParserException {
+		validateType(message, "command", String.class);
+		validateType(message, "signature", String.class);
+		validateType(message, "player_id", Number.class);
+		
+		if(message.get("ack_id") != null) {
+			validateType(message, "ack_id", Number.class);
+		}
+	}
+
+	private static void validateType(JSONObject object, String key, Class<?> class1) throws ParserException {
+		if(object.get(key) != null && class1.isAssignableFrom(object.get(key).getClass())) {
+			return;
+		}
+		
+		throw new ParserException("Cannot parse message. " + key + " must be present and of correct type (" + class1.toString() + ").");
 	}
 }
--- src/test/networking/MessageParserTests.java
+++ src/test/networking/MessageParserTests.java
@@ -1,6 +1,8 @@
 package test.networking;
 
 import static org.junit.Assert.*;
+import networking.Command;
+import networking.Message;
 import networking.parser.Parser;
 import networking.parser.ParserException;
 
@@ -23,17 +25,20 @@ public class MessageParserTests {
 		}
 		*/
 		String message = "{\r\n" + 
-				"			 \"command\": \"acknowledge\",\r\n" + 
+				"			 \"command\": \"acknowledgement\",\r\n" + 
 				"			 \"payload\":	{\r\n" + 
 				"			 				\"ack_id\": 32183921,\r\n" + 
 				"			 				\"response\": 0,\r\n" + 
 				"			 				\"data\": null\r\n" + 
 				"			 			}\r\n" + 
-				"			 \"player_id\": \"5\",\r\n" + 
+				"			 \"player_id\": 5,\r\n" + 
 				"			 \"signature\": \"TBD\"\r\n" + 
 				"		}";
 		
-		assertNotNull(Parser.parseMessage(message));
+		Message msg = Parser.parseMessage(message);
+		
+		assertNotNull(msg);
+		assertEquals(msg.command, Command.ACKNOWLEDGEMENT);
 	}
 	
 	@Test
@@ -49,13 +54,16 @@ public class MessageParserTests {
 		*/
 		String message = "	{\r\n" + 
 				"		 \"command\": \"trade_in_cards\",\r\n" + 
-				"		 \"payload\":	null,\r\n" + 
-				"		 \"player_id\": \"0\",\r\n" + 
+				"		 \"payload\": null,\r\n" + 
+				"		 \"player_id\": 0,\r\n" + 
 				"		 \"signature\": \"TBD\"\r\n" + 
 				"		 \"ack_id\": 67812687\r\n" + 
 				"	}";
 		
-		assertNotNull(Parser.parseMessage(message));
+		Message msg = Parser.parseMessage(message);
+		
+		assertNotNull(msg);
+		assertEquals(msg.command, Command.TRADE_IN_CARDS);
 	}
 	
 	@Test
@@ -69,23 +77,26 @@ public class MessageParserTests {
 			 				"data": null
 			 			}
 			 			
-			 "player_id": "1",
+			 "player_id": 1,
 			 "signature": "TBD"
 		}
 		*/
 		String message = "		{\r\n" + 
-				"		 \"command\": \"acknowledge\",\r\n" + 
+				"		 \"command\": \"acknowledgement\",\r\n" + 
 				"		 \"payload\":	{\r\n" + 
 				"		 				\"ack_id\": 67812687,\r\n" + 
 				"		 				\"response\": 0,\r\n" + 
 				"		 				\"data\": null\r\n" + 
 				"		 			}\r\n" + 
 				"		 			\r\n" + 
-				"		 \"player_id\": \"1\",\r\n" + 
+				"		 \"player_id\": 1,\r\n" + 
 				"		 \"signature\": \"TBD\"\r\n" + 
 				"	}";
 		
-		assertNotNull(Parser.parseMessage(message));
+		Message msg = Parser.parseMessage(message);
+		
+		assertNotNull(msg);
+		assertEquals(msg.command, Command.ACKNOWLEDGEMENT);
 	}
 	
 	@Test
@@ -95,7 +106,7 @@ public class MessageParserTests {
 		    "command": "deploy",
 		    "payload": [[12, 5], [13, 10]],
 		    "ack_id": 12382,
-			"player_id": "0",
+			"player_id": 0,
 			"signature": "TBD"
 		}
 		*/
@@ -103,11 +114,130 @@ public class MessageParserTests {
 				"	    \"command\": \"deploy\",\r\n" + 
 				"	    \"payload\": [[12, 5], [13, 10]],\r\n" + 
 				"	    \"ack_id\": 12382,\r\n" + 
-				"		\"player_id\": \"0\",\r\n" + 
+				"		\"player_id\": 0,\r\n" + 
 				"		\"signature\": \"TBD\"\r\n" + 
 				"	}";
 		
-		assertNotNull(Parser.parseMessage(message));
+		Message msg = Parser.parseMessage(message);
+		
+		assertNotNull(msg);
+		assertEquals(msg.command, Command.DEPLOY);
+	}	
+	@Test
+	public void testAttack() throws ParserException {
+		/*
+		{
+		    "command": "attack",
+		    "payload": [12, 22, 3],
+		    "ack_id": 23342334,
+			"signature": "TBD",
+		    "player_id" : 2
+		}
+		*/
+		String message = "{\r\n" + 
+				"    \"command\": \"attack\",\r\n" + 
+				"    \"payload\": [12, 22, 3],\r\n" + 
+				"    \"ack_id\": 23342334,\r\n" + 
+				"    \"player_id\": 2\r\n" + 
+				"	 \"signature\": \"TBD\"\r\n" + 
+				"}";
+		
+		Message msg = Parser.parseMessage(message);
+		
+		assertNotNull(msg);
+		assertEquals(msg.command, Command.ATTACK);
+	}
+	@Test
+	public void testAttackCapture() throws ParserException {
+		/*
+		{
+		    "command": "attack_capture",
+		    "payload": 5,
+		    "ack_id": 9879876,
+			"signature": "TBD",
+		    "player_id" : 2
+		}
+		*/
+		String message = "{\r\n" + 
+				"    \"command\": \"attack_capture\",\r\n" + 
+				"    \"payload\": 5,\r\n" + 
+				"    \"ack_id\": 9879876,\r\n" + 
+				"    \"player_id\" : 2\r\n" + 
+				"	 \"signature\": \"TBD\"\r\n" + 
+				"}";
+		
+		Message msg = Parser.parseMessage(message);
+		
+		assertNotNull(msg);
+		assertEquals(msg.command, Command.CAPTURE);
+	}
+	@Test
+	public void testFortify() throws ParserException {
+		/*
+		{
+		    "command": "fortify",
+		    "payload": [19, 18, 4],
+		    "ack_id": 4556554,
+				"			 \"signature\": \"TBD\"\r\n" + 
+		    "player_id": 2
+		}
+
+		*/
+		String message = "{\r\n" + 
+				"    \"command\": \"fortify\",\r\n" + 
+				"    \"payload\": [19, 18, 4],\r\n" + 
+				"    \"ack_id\": 4556554,\r\n" + 
+				"    \"player_id\": 2\r\n" + 
+				"	 \"signature\": \"TBD\"\r\n" + 
+				"}\r\n" + 
+				"";
+		
+		Message msg = Parser.parseMessage(message);
+		
+		assertNotNull(msg);
+		assertEquals(msg.command, Command.FORTIFY);
+	}
+	@Test(expected=ParserException.class)
+	public void testNoCommand() throws ParserException {
+		/*
+		{
+		    "command": "fortify",
+		    "ack_id": 4556554,
+			"signature": "TBD",
+		    "player_id": 2
+		}
+
+		*/
+		String message = "{\r\n" + 
+				"    \"ack_id\": 4556554,\r\n" + 
+				"    \"player_id\": 2\r\n" + 
+				"    \"payload\": [19, 18, 4],\r\n" + 
+				"	 \"signature\": \"TBD\"\r\n" + 
+				"}\r\n" + 
+				"";
+		
+		Message msg = Parser.parseMessage(message);
+	}
+
+	@Test(expected=ParserException.class)
+	public void testInvalidCommand() throws ParserException {
+		String message = "{\r\n" + 
+			    "    \"command\": \"Thisisinvalid\"," +
+				"    \"ack_id\": 4556554,\r\n" + 
+				"    \"player_id\": 2\r\n" + 
+				"    \"payload\": [19, 18, 4],\r\n" + 
+				"	 \"signature\": \"TBD\"\r\n" + 
+				"}\r\n" + 
+				"";
+		
+		Message msg = Parser.parseMessage(message);
+	}
+
+	@Test(expected=ParserException.class)
+	public void testJsonMustBeObject() throws ParserException {
+		String message = "[]";
+		
+		Message msg = Parser.parseMessage(message);
 	}
 
 }

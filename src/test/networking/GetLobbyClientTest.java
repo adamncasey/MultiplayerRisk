b98351bb65738a891acbdfb65208db0c260c9de8
Adam Casey <adamncasey@gmail.com>
2014-11-23 06:04:32 +0000

    Finally caved and added the apache commons after so many SO answers.

---
+++ src/test/networking/GetLobbyClientTest.java
@@ -0,0 +1,10 @@
+package test.networking;
+
+import junit.framework.TestCase;
+
+public class GetLobbyClientTest extends TestCase {
+
+    public void testGetLobbyClient() throws Exception {
+
+    }
+}

3130f49cbf8102a555ff981bee73280f0d27ee46
Adam Casey <adamncasey@gmail.com>
2015-02-17 15:25:03 +0000

    Revert "Added the game UI source code and XML files. (big commit)"
    This reverts commit 0d1e30747e7e6cd6e575a6cc73e0feefe42aa948.

---
+++ src/networking/ConnectionLostException.java
@@ -0,0 +1,9 @@
+package networking;
+
+import java.io.IOException;
+
+public class ConnectionLostException extends IOException {
+	
+	private static final long serialVersionUID = -2837902819203315043L;
+
+}
---
+++ src/networking/PortInUseException.java
@@ -0,0 +1,7 @@
+package networking;
+
+public class PortInUseException extends Exception {
+
+	private static final long serialVersionUID = 7369502831402496824L;
+
+}

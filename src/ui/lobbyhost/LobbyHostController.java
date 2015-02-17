475f3a7d3ea7872cbb66d680d6d8299e08310d44
James Russell <jr79@st-andrews.ac.uk>
2015-02-17 17:37:10 +0000

    New LobbyHost screen

---
+++ src/ui/lobbyhost/LobbyHostController.java
@@ -0,0 +1,73 @@
+package ui.creategame;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import lobby.Validator;
+import settings.Settings;
+import ui.*;
+
+public class LobbyHostController extends AnchorPane implements Initializable {
+
+	private Main application;
+
+	@FXML
+	private TextField port;
+	@FXML
+	private TextField players;
+	@FXML
+	private Label status;
+
+	public void setApp(Main application) {
+		this.application = application;
+		this.players.setText("4");
+		this.port.setText(Settings.port + "");
+	}
+
+	private boolean isFormValid() {
+		return isValidPort();
+	}
+
+	private boolean isValidPort() {
+		boolean valid = true;
+
+		if (!Validator.isValidPort(port.getText())) {
+			status("Error: Invalid port");
+			valid = false;
+		}
+
+		return valid;
+	}
+
+	private void status(String message) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				status.setText(message);
+			}
+		});
+	}
+
+
+	@FXML
+	protected void backButtonAction(ActionEvent event) {
+		application.gotoMenu();
+	}
+
+	@FXML
+	protected void startButtonAction(ActionEvent event) {
+		if(isFormValid()) {
+			application.gotoMenu();
+		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {	}
+}

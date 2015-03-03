9bf6a55fd519677a83a87758f38098b27429d147
Adam Casey <adamncasey@gmail.com>
2015-03-03 16:55:22 +0000

    Created NetworkPlayer class

---
+++ src/networking/NetworkPlayer.java
@@ -0,0 +1,105 @@
+package networking;
+
+import java.util.ArrayList;
+
+import logic.Board;
+import logic.Card;
+import logic.Move;
+import player.IPlayer;
+import player.PlayerController;
+
+public class NetworkPlayer implements IPlayer {
+
+	@Override
+	public boolean isEliminated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void eliminate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nextMove(int currentPlayer, String currentMove) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePlayer(Board board, ArrayList<Card> hand,
+			int currentPlayer, Move previousMove) {
+		
+		// This can probably be ignored for a NetworkPlayer
+		// TODO Evaluate this assumption		
+	}
+
+	@Override
+	public Move getMove(Move move) {
+		// Read a message from the network
+		
+		switch(move.getStage()) {
+		case CARD_DRAWN:
+			break;
+		case CHOOSE_ATTACK_DICE:
+			break;
+		case CHOOSE_DEFEND_DICE:
+			break;
+		case CLAIM_TERRITORY:
+			break;
+		case DECIDE_ATTACK:
+			break;
+		case DECIDE_FORTIFY:
+			break;
+		case END_ATTACK:
+			break;
+		case FORTIFY_TERRITORY:
+			break;
+		case GAME_BEGIN:
+			break;
+		case GAME_END:
+			break;
+		case OCCUPY_TERRITORY:
+			break;
+		case PLACE_ARMIES:
+			break;
+		case PLAYER_ELIMINATED:
+			break;
+		case REINFORCE_TERRITORY:
+			break;
+		case SETUP_BEGIN:
+			break;
+		case SETUP_END:
+			break;
+		case START_ATTACK:
+			break;
+		case START_FORTIFY:
+			break;
+		case TRADE_IN_CARDS:
+			break;
+		default:
+			break;
+			
+		}
+		
+		// Apply message information to the move object
+		
+		// return move
+		return null;
+	}
+
+	@Override
+	public int getUID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setUID(int uid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

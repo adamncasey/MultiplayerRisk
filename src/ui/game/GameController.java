9139b5170cf536243b5a896467641e72e71950e9
Victor <va9@st-andrews.ac.uk>
2015-03-19 21:39:48 +0000

    "Fixed" the merge issue. Waiting James' confirmation that everything is actually still working.

--- src/ui/game/GameController.java
+++ src/ui/game/GameController.java
@@ -38,7 +38,7 @@ public class GameController implements Initializable {
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		GameController.console = new GameConsole(consoleTextArea);
-		this.mapControl.initialise();
+		this.mapControl.initialise(console);
 	}
 
 

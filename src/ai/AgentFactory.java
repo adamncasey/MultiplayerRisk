18dafe94c2fd2294b86876cd6439d6734223481a
nb48 <nb48@st-andrews.ac.uk>
2015-03-27 20:06:44 +0000

    Started documenting the AI

--- src/ai/AgentFactory.java
+++ src/ai/AgentFactory.java
@@ -32,28 +32,3 @@ public class AgentFactory {
         return null;
     }
 }
-// Implemented
-
-// Random - Randomly decides what to do
-// Angry - Always attacks, fortifies outwards
-// Greedy - Tries to draw a card every turn (and no more) Trades in cards ASAP
-// Continental - Place initial armies to fill up small continents first,
-// Furious - Always attacks if it has an advantage, also continental
-//
-// Ideas
-
-// Continents2 - Focus efforts towards capturing whole continents
-// Cluster - Cluster expands from his biggest set of armies
-// Yakool - Yakool builds on Cluster by, focusing on enemies that are too strong, slower attacks than cluster, tries to get a card every turn.
-// Boscoe - Boscoe is Yakool with a slowed down attack strategy
-// Bort - Bort is slow and steady, 1 attack per turn, slower than boscoe
-// Communist - Spreads armies evenly among all owned territories, attacks the weakest enemy territory
-// Defendo - Tries to hold on to the territories it starts with, starts by trying to hold a small continent
-// Pixie - Attacks continents that have the fewest border points, fortifies to the front lines
-// Trotsky - Communist with some extra features
-// Vulture - Singles out weak opponents and focuses on eliminating them
-// EvilPixie - Pixie, does not fortify to places where it already has an advantage
-// KillBot - Combines EvilPixie and Vulture behaviour 
-// Shaft - Looks ahead and attacks into as few borders as possible
-// Quo - Shaft that tries to obtain a card every turn
-

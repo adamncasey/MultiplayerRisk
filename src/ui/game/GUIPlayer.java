394a08e089229683f25e7680fe2f816eee3d0d81
James Russell <jr79@st-andrews.ac.uk>
2015-04-17 20:17:59 +0100

    Got shield code back

--- src/ui/game/GUIPlayer.java
+++ src/ui/game/GUIPlayer.java
@@ -21,7 +21,7 @@ public class GUIPlayer implements IPlayer {
 	private GameController gameController;
 	private Board board;
 	private Player player;
-	String lastPlayerToMove;
+	int lastPlayerToMove = -1;
 	private boolean isRealUserPlaying;
 	private String playerName;
 	private MoveChecker moveChecker;
@@ -46,7 +46,7 @@ public class GUIPlayer implements IPlayer {
 		this.moveChecker = checker;
 	}
 
-	public void nextMove(String move) {
+	public void nextMove(String move, int playerID) {
 		GameController.console.write(move);
 
 		Platform.runLater(new Runnable() {
@@ -57,7 +57,7 @@ public class GUIPlayer implements IPlayer {
 		});
 
 		final BorderPane lastShield;
-		if (lastPlayerToMove != null) {
+		if (lastPlayerToMove != -1) {
 			lastShield = gameController.playerShields.get(lastPlayerToMove);
 
 			Platform.runLater(new Runnable() {
@@ -68,7 +68,7 @@ public class GUIPlayer implements IPlayer {
 			});
 		}
 
-		final BorderPane shield = gameController.playerShields.get(playerName);
+		final BorderPane shield = gameController.playerShields.get(playerID);
 		Platform.runLater(new Runnable() {
 			@Override
 			public void run() {
@@ -76,7 +76,7 @@ public class GUIPlayer implements IPlayer {
 			}
 		});
 
-		lastPlayerToMove = playerName;
+		lastPlayerToMove = playerID;
 	}
 
 	public void updatePlayer(Move move) {

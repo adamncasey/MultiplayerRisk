1646cbbbef38a7f74cf614fee582bd738e737523
James Russell <jr79@st-andrews.ac.uk>
2015-04-19 18:31:14 +0100

    Cards control uses scroll pane / flow pane correctly - good for resizing

--- src/ui/game/cards/CardControl.java
+++ src/ui/game/cards/CardControl.java
@@ -1,15 +1,24 @@
 package ui.game.cards;
 
 import java.io.IOException;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Bounds;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
 
 public class CardControl extends GridPane {
 
 	@FXML
 	Label title;
+	@FXML
+	ScrollPane root;
+	@FXML
+	FlowPane flowPane;
 
 	public CardControl() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
@@ -21,6 +30,14 @@ public class CardControl extends GridPane {
 		} catch (IOException exception) {
 			throw new RuntimeException(exception);
 		}
+		
+		root.viewportBoundsProperty().addListener(new ChangeListener<Bounds>() {
+            @Override
+            public void changed(ObservableValue<? extends Bounds> ov, Bounds oldBounds, Bounds bounds) {
+            	flowPane.setPrefWidth(bounds.getWidth());
+            	flowPane.setPrefHeight(bounds.getHeight());
+            }
+        });
 	}
 
 	@FXML

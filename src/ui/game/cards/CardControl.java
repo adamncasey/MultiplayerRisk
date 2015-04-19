5e677b1433ae99a72e9b0a4520d325922fbcb208
James Russell <jr79@st-andrews.ac.uk>
2015-04-20 00:17:29 +0100

    Can select 3 cards and make trade in cards button visible.

--- src/ui/game/cards/CardControl.java
+++ src/ui/game/cards/CardControl.java
@@ -1,21 +1,28 @@
 package ui.game.cards;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import ui.game.map.MapControl;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Bounds;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.VPos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import logic.Card;
 
@@ -27,10 +34,17 @@ public class CardControl extends GridPane {
 	ScrollPane root;
 	@FXML
 	FlowPane flowPane;
+	@FXML
+	Button submit;
 	
 	Image artilleryImage, cavalryImage, infantryImage;
 	
 	MapControl map;
+	
+	Map<GridPane, Card> hand = new HashMap<GridPane, Card>();
+	Map<Card, Region> selectRegions = new HashMap<Card, Region>();
+	
+	Set<GridPane> selected = new HashSet<GridPane>();
 
 	public CardControl() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
@@ -52,6 +66,10 @@ public class CardControl extends GridPane {
         });
 	}
 	
+	// ================================================================================
+	// Create / Add cards
+	// ================================================================================
+	
 	public void initialise(MapControl map) {
 		this.map = map;
 		artilleryImage = new Image(getClass().getResourceAsStream("resources/artillery.jpg"));
@@ -59,21 +77,40 @@ public class CardControl extends GridPane {
 		infantryImage = new Image(getClass().getResourceAsStream("resources/infantry.jpg"));
 	}
 	
-	
 	public void add(Card card) {
+		GridPane pane = getCard(card);
+		hand.put(pane, card);
+		
 		Platform.runLater(new Runnable() {
 			@Override
 			public void run() {
-				flowPane.getChildren().add(getCard(card));
+				flowPane.getChildren().add(pane);
 			}
 		});
+		
+		pane.addEventFilter(MouseEvent.MOUSE_CLICKED,
+				new EventHandler<MouseEvent>() {
+					@Override
+					public void handle(MouseEvent mouseEvent) {
+						cardClicked((GridPane)mouseEvent.getSource(), card);
+					}
+				});
 	}
 
 	private GridPane getCard(Card card) {
 		GridPane grid = new GridPane();
 		grid.setPrefSize(99, 150);
 		
-		ImageView background = new ImageView(artilleryImage);
+		ImageView background = null;
+		
+        if(card.getType() == 1){
+			background = new ImageView(infantryImage);
+        }else if(card.getType() == 5){
+        	background = new ImageView(cavalryImage);
+        }else if(card.getType() == 10){
+        	background = new ImageView(artilleryImage);
+        }
+		
 		background.setFitHeight(150);
 		background.setPickOnBounds(true);
 		background.setPreserveRatio(true);
@@ -94,15 +131,53 @@ public class CardControl extends GridPane {
 		GridPane.setValignment(territory, VPos.BOTTOM);
 		GridPane.setMargin(territory, new Insets(0,0,8,0));
 		
+		Region blackout = new Region();
+		blackout.getStyleClass().add("blackout");
+		selectRegions.put(card, blackout);
+		
 		grid.add(background, 0, 0);
 		grid.add(name, 0, 0);
 		grid.add(territory, 0, 0);
+		grid.add(blackout, 0, 0);
+		grid.getStyleClass().add("card");
 		
 		return grid;
 	}
+	
+	private void cardClicked(GridPane pane, Card card){
+		if(selected.contains(pane)) {
+			if(selected.size() == 3) {
+				submit.setVisible(false);
+			}
+			unselectCard(pane);
+		}
+		else if(selected.size() < 3){
+			selectCard(pane, card);
+		}
+		
+		if(selected.size() == 3) {
+			submit.setVisible(true);
+		}
+	}
+	
+	private void selectCard(GridPane pane, Card card) {
+		pane.getStyleClass().add("cardSelected");
+		Region r = selectRegions.get(card);
+		r.getStyleClass().clear();
+		r.getStyleClass().add("selected");
+		selected.add(pane);
+	}
+	
+	private void unselectCard(GridPane pane) {
+		pane.getStyleClass().add("cardSelected");
+		Region r = selectRegions.get(hand.get(pane));
+		r.getStyleClass().clear();
+		r.getStyleClass().add("blackout");
+		selected.remove(pane);
+	}
 
 	@FXML
-	protected void submit() {
+	protected void submit(MouseEvent mouseEvent) {
 		
 	}
 }
